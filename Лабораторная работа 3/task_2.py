95235cef0ffab56215280aced48ec71f643cad5f
Jam1337 <crypto11925@gmail.com>
2024-12-08 23:42:53 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task_2.py
@@ -0,0 +1,25 @@
+# TODO Напишите функцию find_common_participants
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+# TODO Провеьте работу функции с разделителем отличным от запятой
+# print("Общие участники среди двух групп:", participants_first_group.intersection)
+
+def find_common_participants(group1, group2, n=','):
+    first = set(group1.split(n))
+    second = set(group2.split(n))
+    common_participants = first.intersection(second)
+    return sorted(common_participants)
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+common_participants = find_common_participants(participants_first_group, participants_second_group)
+print(common_participants)
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+print(find_common_participants(participants_first_group, participants_second_group))
